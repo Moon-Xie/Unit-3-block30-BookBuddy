65081c3b0316e32fc78437d16223bc5b7dfd927c
Yue Xie <moonxie9304@gmail.com>
2025-02-26 01:20:56 +0700

    add basic code for Books component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { useState } from 'react'
 import bookLogo from './assets/books.png'
+import Books from './components/Books'
 
 function App() {
   const [token, setToken] = useState(null)
-
+  const [books, setBooks] = useState([])
+  console.log('books are =>', books)
   return (
     <>
-      <h1><img id='logo-image' src={bookLogo}/>Library App</h1>
-
-      <p>Complete the React components needed to allow users to browse a library catalog, check out books, review their account, and return books that they've finished reading.</p>
-
-      <p>You may need to use the `token` in this top-level component in other components that need to know if a user has logged in or not.</p>
-
-      <p>Don't forget to set up React Router to navigate between the different views of your single page application!</p>
+      <nav className='navBar'> 
+        <h1><img id='logo-image' src={bookLogo}/>Library App</h1>
+        <h1>Filter function here</h1>
+        <h3>login/sign up</h3>
+      </nav>
+      <div className='bookCards'>
+        <Books setBooks={setBooks}/>
+      </div>
     </>
   )
 }
